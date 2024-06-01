ac1fd112572a3adb1548b974ff0625e910568379
Jbean <jbsbin84@gmail.com>
2024-06-01 13:17:26 +0900

    edit Resume.tsx

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,5 +1,22 @@
 const Resume = () => {
-  return <div>나는 프로젝트 내역</div>;
+  return (
+    <div>
+      <div>
+        <h2>실전코딩 수강 </h2>
+      </div>
+      <div>2023.03 ~ 2023.06</div>
+      <div> React, Spring 등 다양한 실무 관련 지식 수강</div>
+      <div></div>
+      <div>
+        <h2>웹프로그래밍 수업 Term Project</h2>
+        <div>jQuery를 이용하여 스터디 플레너를 구현함. react가 어렵지만 더 짱짱같아 보여요</div>
+      </div>
+      <div>
+        <h2>현재 평점</h2>
+        전공 3.9, 전체 3.75
+      </div>
+    </div>
+  );
 };
 
 export default Resume;
